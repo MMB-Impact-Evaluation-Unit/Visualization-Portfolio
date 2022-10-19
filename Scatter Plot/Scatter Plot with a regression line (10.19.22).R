6b53210a9e625a91ba9ccd667f66a180fb04d01b
Impact Evaluation - Minnesota Management and Budget <57459306+mmbimpact@users.noreply.github.com>
2022-10-19 08:27:41 -0500

    Add files via upload

---
+++ Scatter Plot/Scatter Plot with a regression line (10.19.22).R
@@ -0,0 +1,37 @@
+# Stacked Bar Chart
+## Author : Yuna Loesch
+## 10.19.22
+
+library(tidyverse)
+library(mmbtools)
+library(skimr)
+library(scales)
+mmb_load_fonts()
+
+# sample dataset : "iris"
+## take a peek at the dataset
+glimpse(iris)
+skim(iris)
+summary(iris)
+
+
+# scatter plot chart
+iris %>% 
+  ggplot(aes(x = Petal.Length, y = Petal.Width, color = Species, shape = Species)) + 
+  geom_point() +
+  geom_smooth() + #add a regression line
+  theme_mmb_basic() + #apply mmb theme
+  scale_color_mncol(palette = "main") + #apply MN theme color
+  scale_x_continuous(breaks = seq(0, 7, by = 0.5)) + # manually assign grid by 0.5
+  labs(
+    title = "Petal length/width of Iris",
+    subtitle = ("Species : Setosa/Versicolor/Virginica"),
+    x = "Length of Petal",
+    y = "Width of Petal"
+    ) +
+  theme(plot.title = element_text(hjust = 0.5), #center title
+        plot.subtitle = element_text(hjust = 1), #subtitle on the right
+       
+  )
+
+  
